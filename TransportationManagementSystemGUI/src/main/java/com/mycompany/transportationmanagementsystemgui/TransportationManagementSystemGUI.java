7e88fcd97324de4057ea0627d699067fbece16a6
Esma222 <106523107+Esma222@users.noreply.github.com>
2023-07-02 22:51:20 +0300

    Transportation Ticket App
    Transportation Ticket App

---
+++ TransportationManagementSystemGUI/src/main/java/com/mycompany/transportationmanagementsystemgui/TransportationManagementSystemGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transportationmanagementsystemgui;
+
+/**
+ *
+ * @author Btrlp8
+ */
+public class TransportationManagementSystemGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
